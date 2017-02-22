0c23b37c234d820c6ffac4f81262e54cd9d1d8b7
OlegPorodkin <stixlive59@gmail.com>
2017-02-22 22:21:32 +0300

    add my assi

---
+++ курсоваяРабота/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсоваяРабота
+{
+    class Program
+    {
+        static double intFunk(double x)
+        {
+            return (Math.Pow(x, 2) * Math.Sqrt(4 - Math.Pow(x, 2)));
+        }
+
+        static double calcIntegral(double a, double b, double n)
+        {
+            double h, result;
+
+            result = 0.0;
+
+            h = (b - a) / n;
+
+            for (int i = 0; i < n; i++)
+            {
+                result += intFunk(a + h * (i + 0.5));
+            }
+            result *= h;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            double integral;
+
+            int a1, b1, n1;
+
+            Console.Write("Введите нижний предел: ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите верхний предел: ");
+            b1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите шаг: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            integral = calcIntegral(a1, b1, n1);
+            Console.WriteLine("Значение интеграла = {0}", integral);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ курсоваяРабота/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("курсоваяРабота")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("курсоваяРабота")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("87102400-89fa-4fa1-bb18-0c3197f727df")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
